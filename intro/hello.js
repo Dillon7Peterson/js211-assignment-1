aff975700d34a2edf804c9d81c87e3c1fc560f52
Dillon7Peterson <Dillon7peterson@gmail.com>
2020-06-02 10:52:29 -0500

    pushing mods

--- intro/hello.js
+++ intro/hello.js
@@ -7,3 +7,17 @@ let y = 8;
 let z = y+x;
 
 console.log('the sum of z is', z);
+
+
+
+
+let letterGrade = (actualGrade) => {
+  let letter =;
+
+  if (actualGrade <= 90) {
+    letter = 'A';
+
+    if (actualGrade >= 80 && <= 89);
+    
+  }
+}
